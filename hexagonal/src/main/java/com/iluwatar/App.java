ba3c06857866bc48534e5b3c136457aa278dfa78
Ilkka Seppala <iluwatar@gmail.com>
2015-10-08 20:20:38 +0300

    Apply google style#86

--- hexagonal/src/main/java/com/iluwatar/App.java
+++ hexagonal/src/main/java/com/iluwatar/App.java
@@ -1,7 +1,7 @@
 package com.iluwatar;
 
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
